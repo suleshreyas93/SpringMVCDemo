e4cc27b1308d583ef3b71d05e08ddd574de8d073
Shreyas Sule <suleshreyas7@gmail.com>
2017-08-03 19:29:17 -0400

    Added Controller

---
+++ src/controllers/OffersController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OffersController {
+	
+	@RequestMapping("/")
+	public String showOffers(Model model)
+	{
+		model.addAttribute("offer", "Offers are as follows : ");
+		return "offer";
+	}
+
+}
